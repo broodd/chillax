42304b197659df5d6475010a5be5cf7ba758310c
Svyat <pidgurdkiy2000@gmail.com>
2019-11-04 23:54:34 +0200

    wellcome, home, login, reg

--- app/app.js
+++ app/app.js
@@ -1,19 +1,66 @@
 import Vue from "nativescript-vue";
 import store from './store';
+// const firebase = require("nativescript-plugin-firebase");
+// const firebaseWebApi = require("nativescript-plugin-firebase/app");
 
-import Home from "./components/Home";
+// firebase.init({
+// 	// Optionally pass in properties for database, authentication and cloud messaging,
+// 	// see their respective docs.
+// }).then(
+// 	function () {
+// 		console.log("firebase.init done");
+// 		console.log("[*] Firebase was successfully initialised");
+// 	},
+// 	function (error) {
+// 		console.log("firebase.init error: " + error);
+// 	}
+// );
 
 // var authService = require("./auth-service");
 // authService.configureOAuthProviders();
+
+
+import Wellcome from "./components/Wellcome";
+import Home from "./components/Home";
+
+// import Login from "./components/Login";
 
 new Vue({
 	template: `
 		<Frame>
 				<Home />
-		</Frame>`,
+		</Frame>
+	`,
 	components: {
-		Home
+		Wellcome,
+		Home,
+		// Login
 	},
 	// render: h => h(Home),
 	store,
 }).$start();
+
+Vue.config.silent = true;
+
+{/* <BottomNavigation>
+	<TabStrip>
+		<TabStripItem>
+			<Label text="Home"></Label>
+		</TabStripItem>
+		<TabStripItem>
+			<Label text="Login"></Label>
+		</TabStripItem>
+	</TabStrip>
+
+	<TabContentItem>
+		<Frame>
+			<Wellcome />
+		</Frame>
+	</TabContentItem>
+
+	<TabContentItem>
+		<Frame>
+			<Login />
+		</Frame>
+	</TabContentItem>
+</BottomNavigation> */}
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,50 +1,42 @@
 <template>
-    <Page class="page page--home c-white" actionBarHidden="true" backgroundSpanUnderStatusBar="true">
+    <Page class="page page--home c-black" actionBarHidden="true" backgroundSpanUnderStatusBar="true">
 			<ScrollView class="panel panel--home" orientation="vertical">
-				<GridLayout class="form bg bg--main" :class="{'form-active': state == 'home'}" columns="*" rows="*, auto">
+				<GridLayout class="" columns="*" rows="200, auto">
 					
-					<FlexboxLayout class="form__top container" width="100%" row="0">
-						<StackLayout class="row text-center" height="100%">
-							<FlexboxLayout alignItems="center" justifyContent="center" height="100%">
-								<Image src="~/assets/img/logo.png"/>
+					<FlexboxLayout class="container container-fluid" width="100%" row="0">
+						<StackLayout class="row" height="100%">
+							<FlexboxLayout flexDirection="column" justifyContent="flex-end" height="100%">
+								<Label class="fz-35" text="Popular playlists" @tap="$navigateTo(Wellcome)"/>
+								<Label class="fz-17" text="Good Morning"/>
+								<!-- <Image src="~/assets/img/logo.png"/> -->
 							</FlexboxLayout>
 						</StackLayout>
 					</FlexboxLayout>
 
 
-					<FlexboxLayout class="form__bottom container pb-4" width="100%" row="1">
-						<StackLayout class="row text-center">
-							<Label :text="token" />
+					<FlexboxLayout class="container container-fluid" width="100%" row="1">
+						<StackLayout class="row">
+							<Label :text="playListScroll"></Label>
+							<Label :text="maxPlayListScroll"></Label>
+							<ScrollView ref="playlist_list" class="playlist-list" orientation="horizontal" scrollBarIndicatorVisible="false" @scroll="onPlayListScroll">
+								<StackLayout orientation="horizontal">
+									<Button class="playlist-list__btn" text="Focus"/>
+									<Button class="playlist-list__btn" text="Focus"/>
+									<Button class="playlist-list__btn" text="Focus"/>
+									<Button class="playlist-list__btn" text="Focus"/>
+									<Button class="playlist-list__btn" text="Focus"/>
+									<Button class="playlist-list__btn" text="Focus"/>
+								</StackLayout>
+							</ScrollView>
+							<!-- <Label :text="state"></Label>
 							<Button class="btn" text="Sign in" @tap="$navigateTo(Login)" />
 							<Button class="btn green" text="Sign in with google" @tap="login('google')" />
 							<Button class="btn blue" text="Sign in with facebook" @tap="login('facebook')" />
 							<Button class="btn tomato" text="Log out" @tap="logOut()" />
 							
-							<Label class="mt-2 new-account" text="New Account?" @tap="$navigateTo(Login)" />
+							<Label class="mt-2 new-account" text="New Account?" @tap="$navigateTo(Login)" /> -->
 						</StackLayout>
 					</FlexboxLayout>
-
-					<!-- <FlexboxLayout v-if="state == 'home'" class="form__bottom container pb-4" width="100%" row="1">
-						<StackLayout class="row text-center">
-							<Button class="btn" text="Sign in" @tap="changeState('login')" />
-							<Button class="btn green" text="Sign in with google" @tap="changeState('login')" />
-							<Button class="btn blue" text="Sign in with facebook" @tap="changeState('login')" />
-							
-							<Label class="mt-2 new-account" text="New Account?" @tap="changeState('login')" />
-						</StackLayout>
-					</FlexboxLayout>
-					<FlexboxLayout v-if="state == 'login'" class="form__bottom container pb-4" width="100%" row="1">
-						<StackLayout class="row text-center">
-							<Button class="close my-fa" text.decode="&#xe801;" @tap="changeState('home')" />
-
-							<TextField class="field" v-model="email" hint="EMAIL" />
-							<TextField class="field" v-model="email" hint="PASSWORD" />
-
-							<Button class="btn shadow" text="Sign in" @tap="changeState('home')" />
-							
-							<Label class="mt-2 new-account" text="New Account?" @tap="changeState('home')" />
-						</StackLayout>
-					</FlexboxLayout> -->
 
 				</GridLayout>
 			</ScrollView>
@@ -52,38 +44,35 @@
 </template>
 
 <script>
-		import Login from '@/components/Login';
-		// var authService = require("@/auth-service");
-		
+		import Wellcome from '@/components/Wellcome';
+
 
     export default {
 			computed: {
-				token () {
-					return Object.keys(this.$store.getters.getToken || {}).join(' ')
-					// return this.$store.getters.getToken
-				}
 			},
 			data() {
 				return {
-					Login,
-					state: 'home'
+					Wellcome,
+					maxPlayListScroll: (5 * (160 + 10)) - 300,
+					playListScroll: 0
 				}
 			},
 			methods: {
-				changeState (state) {
-					this.state = state
-				},
-				login (type) {
-					
-
-					// authService.tnsOauthLogin(type)
-					// 	.then((token) => {
-					// 		this.$store.dispatch('setToken', token)
-					// 	});
-				},
-				logOut () {
-					// authService.tnsOauthLogout();
+				onPlayListScroll (args) {
+					let scroll = args.scrollX
+					let elements = 6
+					this.playListScroll = scroll
+					if (scroll >= this.maxPlayListScroll) {
+						alert({
+							title: "Your title",
+							message: "Your message",
+							okButtonText: "Your OK button text"
+						})
+						this.maxPlayListScroll += (5 * (160 + 10)) - 300
+					}
 				}
+			},
+			created () {
 			}
     };
 </script>
@@ -94,4 +83,21 @@
     // End custom common variables
 
 		// Custom styles
+		.playlist-list {
+			&__btn {
+				margin-right: 10;
+				margin-left: 0;
+				text-transform: uppercase;
+				text-align: center;
+				width: 160;
+				height: 160;
+				font-size: 22;
+				letter-spacing: 0.2;
+				color: $white;
+				// border-radius: 10;
+				background-size: cover;
+				background-image: url('~/assets/img/playlists/focus.jpg');
+				// background-color: lime;
+			}
+		}
 </style>
